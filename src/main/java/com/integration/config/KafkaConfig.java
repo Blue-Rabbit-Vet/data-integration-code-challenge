697941e092a8ab61126a5801c068f02d55f8d810
la.rwais <robert.wais@covetrus.com>
2022-11-21 08:27:11 -0600

    Clean Up
    Remove commented code and unused packages

--- src/main/java/com/integration/config/KafkaConfig.java
+++ src/main/java/com/integration/config/KafkaConfig.java
@@ -46,7 +46,6 @@ public class KafkaConfig {
     public ConsumerFactory<String, Object> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap);
-//        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(props);
--- src/main/java/com/integration/controller/PlayerController.java
+++ src/main/java/com/integration/controller/PlayerController.java
@@ -2,7 +2,6 @@ package com.integration.controller;
 
 import com.integration.service.PlayerService;
 import com.integration.dto.Player;
-import com.integration.service.PublishKafkaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
